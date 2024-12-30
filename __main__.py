c655bf9fa0d2f4a8c6d604748f5dd054818893c1
Sindre Saeter <saeter.sindre@gmail.com>
2024-12-30 18:53:42 +0100

    Final program structure added

--- __main__.py
+++ __main__.py
@@ -9,3 +9,14 @@ class Block:
         self.current_hash = None
         self.previous_hash = None
         self.nonce = None
+
+class TheoryCoin:
+    def __init__(self):
+        self.tcchain = []
+
+class BlockchainInteraction:
+    pass
+
+if __name__ == "__main__":
+    program = BlockchainInteraction()
+    program.run()
