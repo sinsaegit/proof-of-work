f9a9b9c5791b0b67077b957b6e36fdd1b3e371aa
Sindre Saeter <saeter.sindre@gmail.com>
2024-12-30 18:56:25 +0100

    Block adding features added, including genesis

--- __main__.py
+++ __main__.py
@@ -14,6 +14,34 @@ class TheoryCoin:
     def __init__(self):
         self.tcchain = []
 
+    def add_block(self):
+        if not self.tcchain:
+            self.tcchain.append(self.add_genesis_block())
+        else:
+            block = Block()
+            block.transaction = self.add_transactions()
+            block.height = len(self.tcchain)
+            block.previous_hash = self.tcchain[-1].current_hash
+            hash_res = self.add_hash(block)
+            pow_res = self.proof_of_work(hash_res)
+            block.current_hash = pow_res[0]
+            block.nonce = pow_res[1]
+            self.tcchain.append(block)
+
+
+    def add_genesis_block(self):
+        genesis = Block()
+        genesis.height = 0
+        genesis.previous_hash = "0"*64
+        genesis.transaction = self.add_transactions()
+        hash_res = self.add_hash(genesis)
+        pow_res = self.proof_of_work(hash_res)
+        genesis.current_hash = pow_res[0]
+        genesis.nonce = pow_res[1]
+        return genesis
+    
+
+    
 class BlockchainInteraction:
     pass
 
